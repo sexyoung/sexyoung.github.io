6ad9d2a09860b8e8124f66353b806cc35c107465
sexyoung <Habuche@gmail.com>
2018-05-25 01:01:41 +0800

    首頁重設計

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <Home />
         <ul className="menu">
-          <li><a href="#">about</a></li>
-          <li><a href="#">service</a></li>
-          <li><a href="#">projects</a></li>
-          <li><a href="#">directors</a></li>
-          <li><a href="#">supplies</a></li>
-          <li><a href="#">guarantee</a></li>
-          <li><a href="#">testimonials</a></li>
-          <li><a href="#">contact</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Service</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Directors</a></li>
+          <li><a href="#">Supplies</a></li>
+          <li><a href="#">Guarantee</a></li>
+          <li><a href="#">Testimonials</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
+        <Home />
         <Service />
         <Project />
         <Director />
--- src/blocks/Home/index.js
+++ src/blocks/Home/index.js
@@ -4,15 +4,13 @@ class HomeBlock extends React.PureComponent {
   render () {
     return (
       <div className="block home-block">
-        <div className="slogan">
-          <div className="main-text">
-            P R O P E R T Y<br/>
-            D E V E L O P M E N T<br/>
-            C O N S T R U S T I O N
-          </div>
-          <div className="sub-text">BASED IN MELBOURNE</div>
-        </div>
         <div className="logo" />
+        <ul className="slogan">
+          <li>Property</li>
+          <li>Development</li>
+          <li>Investment</li>
+          <li>Construction</li>
+        </ul>
         <div className="coverflow"></div>
       </div>
     );
