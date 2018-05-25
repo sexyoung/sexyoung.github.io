d92a18a2b336c30ec2cdb16cbc995664a058569b
sexyoung <Habuche@gmail.com>
2018-05-25 12:10:39 +0800

    Director block

--- src/blocks/Director/index.js
+++ src/blocks/Director/index.js
@@ -4,7 +4,19 @@ class Director extends React.PureComponent {
   render () {
     return (
       <div className="block director-block">
-        <h2>directors-block</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 title">
+              Diretctor
+            </div>
+            <div className="col-md-6 text padding">
+              <div className="sub-title">Eddy Scheruga</div>
+              <p>Designers, Registered Builders and for local + investors.</p>
+              <p>Property developers, Local and international Scheracon connects profitable investors to award winning and property developments in Australia</p>
+              <p>Property developers, Local and international Scheracon connects profitable investors to award winning and property .</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
