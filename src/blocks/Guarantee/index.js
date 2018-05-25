84eb93797e27ae38a5834c6fb04f06cd1eedb1ef
sexyoung <Habuche@gmail.com>
2018-05-25 12:25:35 +0800

    Guarantee block

--- src/blocks/Guarantee/index.js
+++ src/blocks/Guarantee/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import master from './images/Master-Builders-logo.jpg';
+import registered from './images/Registered-Bilding-Practitioner.jpg';
+import vba from './images/VBA.jpg';
+import yarrington from './images/yarrington-construction-bendigo-3550-accreditation.png';
 
 class Guarantee extends React.PureComponent {
   render () {
     return (
       <div className="block guarantee-block">
-        <h2>guarantee-block</h2>
+        <div className="container">
+          <div className="sub-title">Quality Certificated</div>
+          <div className="row">
+            <div className="col-md-3" style={{backgroundImage: `url(${master})`}} />
+            <div className="col-md-3" style={{backgroundImage: `url(${registered})`}} />
+            <div className="col-md-3" style={{backgroundImage: `url(${vba})`}} />
+            <div className="col-md-3" style={{backgroundImage: `url(${yarrington})`}} />
+          </div>
+        </div>
       </div>
     );
   }
