a2c47c9f3a2d9fa975af397e8045ce1d10b99c6b
sexyoung <Habuche@gmail.com>
2018-05-25 10:51:30 +0800

    service 做好

--- src/blocks/Service/index.js
+++ src/blocks/Service/index.js
@@ -4,7 +4,29 @@ class Service extends React.PureComponent {
   render () {
     return (
       <div className="block service-block">
-        <h2>what-we-do-block</h2>
+        <div className="container">
+          <h2 className="title">What do we do?</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="partnerships">
+                Partnerships
+                <a href="#" target="_blank" className="more">more</a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="immigration-assistance">
+                Immigration Assistance
+                <a href="#" target="_blank" className="more">more</a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="property-development">
+                Property Development
+                <a href="#" target="_blank" className="more">more</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
