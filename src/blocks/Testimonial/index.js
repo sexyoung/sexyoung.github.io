48a714e5a8de2e9e852b4808c28a1109b5ed9f4a
sexyoung <Habuche@gmail.com>
2018-05-25 13:08:35 +0800

    testimonial block

--- src/blocks/Testimonial/index.js
+++ src/blocks/Testimonial/index.js
@@ -4,7 +4,23 @@ class Testimonial extends React.PureComponent {
   render () {
     return (
       <div className="block testimonial-block">
-        <h2>testimonials-block</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 title">
+              What Our Customers Says
+            </div>
+            <div className="col-md-6 text">
+              <div className="sub-title">Katie Chou</div>
+              <div className="detail">
+                <p>Property developers, Local and international</p>
+                <p>Scheracon connects profitable investors to award winning and property developments in Australia.</p>
+                <p>Scheracon connects profitable investors to award winning and property developments in Australia</p>
+                <p>Scheracon connects profitable investors to award winning and property developments in Australia...... </p>
+                <a href="#" target="_blank" className="more">more</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
